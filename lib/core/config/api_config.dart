bebe99ba06dc706f8fb65ad028ae90084b15d985
Jfer178 <jeremyazzfontalvomunera@gmail.com>
2026-04-10 17:26:50 -0500

    URLServidorSwicth

--- lib/core/config/api_config.dart
+++ lib/core/config/api_config.dart
@@ -1,7 +1,7 @@
 class ApiConfig {
   // true: usa Docker local (http://localhost:3000)
   // false: usa servidor universidad (http://190.143.117.179:8080)
-  static const bool useLocalDocker = true;
+  static const bool useLocalDocker = false;
 
   static const String localBaseUrl = 'http://localhost:3000';
   static const String universityBaseUrl = 'http://190.143.117.179:8080';
