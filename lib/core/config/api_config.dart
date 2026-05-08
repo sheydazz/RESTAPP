50d7b99bf523bd2550385ac7819325728f1840b4
Jfer178 <jeremyazzfontalvomunera@gmail.com>
2026-05-08 12:26:43 -0500

    CambioServidor

--- lib/core/config/api_config.dart
+++ lib/core/config/api_config.dart
@@ -3,7 +3,7 @@ import 'package:flutter/foundation.dart';
 
 class ApiConfig {
   // true: usa Docker local
-  // false: usa servidor universidad (http://190.143.117.179:8080)
+  // false: usa servidor universidad (http://190.143.117.179:3000)
   static const bool useLocalDocker = false;
 
   // Local host mapping by platform
@@ -12,7 +12,7 @@ class ApiConfig {
   static const String localBaseUrl = 'http://localhost:3000';
   static const String androidEmulatorBaseUrl = 'http://10.0.2.2:3000';
 
-  static const String universityBaseUrl = 'http://190.143.117.179:8080';
+  static const String universityBaseUrl = 'http://190.143.117.179:3000';
 
   static String get baseUrl {
     if (!useLocalDocker) {
