37e82f2b4b77537e5bb17d8bb983b7903b617013
Jfer178 <jeremyazzfontalvomunera@gmail.com>
2026-05-07 23:41:21 -0500

    Local Switch

--- lib/core/config/api_config.dart
+++ lib/core/config/api_config.dart
@@ -4,7 +4,7 @@ import 'package:flutter/foundation.dart';
 class ApiConfig {
   // true: usa Docker local
   // false: usa servidor universidad (http://190.143.117.179:8080)
-  static const bool useLocalDocker = true;
+  static const bool useLocalDocker = false;
 
   // Local host mapping by platform
   // Android emulator: 10.0.2.2 -> host machine
