e970c9439ddc10304525af38932e97e5940cb70a
ARMANDO DE JESÚS PÉREZ BOLÍVAR <armandod-perezb@unilibre.edu.co>
2026-08-05 11:07:04 -0500

    Cambio de ip

--- lib/core/config/api_config.dart
+++ lib/core/config/api_config.dart
@@ -12,7 +12,7 @@ class ApiConfig {
   static const String localBaseUrl = 'http://localhost:3000';
   static const String androidEmulatorBaseUrl = 'http://10.0.2.2:3000';
 
-  static const String universityBaseUrl = 'http://10.4.100.146:3000';
+  static const String universityBaseUrl = 'http://179.197.239.216:3000';
 
   static String get baseUrl {
     if (!useLocalDocker) {
